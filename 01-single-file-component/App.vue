3a8118a80937dbf519cafd5d560a7a653ab5a169
Paulo Valadares <contato@paulovaladares.com.br>
2021-01-27 21:41:41 -0300

    Fix aula 01

--- 01-single-file-component/App.vue
+++ 01-single-file-component/App.vue
@@ -1,4 +1,18 @@
 <template>
+<div>
 <h1>Funfou</h1>
 <p>running with `vue serve`</p>
+</div>
 </template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+h1 {
+    color:blue;
+}
+</style>
